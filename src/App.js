b9b197c4e6e2491356728aa279f7c2c53bc00cc9
Kamasah Edwin Jnr <102263848+derrysnr@users.noreply.github.com>
2024-08-13 15:13:31 +0000

    added card component
    added a card component to make the app lively

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import Header from "./components/Header";
-
+import FeedbackList from "./components/FeedbackList";
+import { useState } from "react";
+import FeedbackData from "./data/FeedbackData";
 
 function App() {
+    const [feedback, setFeedback] = useState(FeedbackData )
     return (
         <>
           <Header />
           <div className="container">
-            <h1>My App</h1>
+            <FeedbackList feedback={feedback} />
           </div>
         </>
     )
