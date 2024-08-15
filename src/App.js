05b3b18bcd77d0f68d46b8a39963ea6710fd97a6
Kamasah Edwin Jnr <kamassahe5@gmail.com>
2024-08-15 15:52:40 +0000

    Feedback stats component
    added a new component that calculates and output the stats of the ratings

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import Header from "./components/Header";
 import FeedbackList from "./components/FeedbackList";
+import FeedbackStats from "./components/FeedbackStats";
 import { useState } from "react";
 import FeedbackData from "./data/FeedbackData";
 
@@ -14,6 +15,7 @@ function App() {
         <>
           <Header />
           <div className="container">
+            <FeedbackStats feedback={feedback}/>
             <FeedbackList feedback={feedback} 
             handleDelete={deleteFeedback}
             />
