c4dd39567991d15a7dc8c59c1eaed2d7620ce7dc
Kamasah Edwin Jnr <kamassahe5@gmail.com>
2024-08-21 11:06:15 +0000

    Rating Selector
    added a a rating component with a function that makes selecting the rating points possible

--- src/App.js
+++ src/App.js
@@ -7,6 +7,11 @@ import FeedbackForm from "./components/FeedbackForm";
 
 function App() {
     const [feedback, setFeedback] = useState(FeedbackData)
+
+    const addFeedback = (newFeedback) => {
+      console.log(newFeedback)
+    }
+
     const deleteFeedback = (id) => {
     if (window.confirm('Are you sure you want to delete this feedback?')) {
       setFeedback(feedback.filter((item) => item.id !== id ))
@@ -16,7 +21,7 @@ function App() {
         <>
           <Header />
           <div className="container">
-            <FeedbackForm />
+            <FeedbackForm handleAdd={addFeedback}/>
             <FeedbackStats feedback={feedback}/>
             <FeedbackList feedback={feedback} 
             handleDelete={deleteFeedback}
--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -1,9 +1,11 @@
 import { useState } from "react"
+import RatingSelect from "./RatingSelect"
 import Card from "./shared/Card"
 import Button from "./shared/Button"
 
-function FeedbackForm() {
+function FeedbackForm( handleAdd) {
   const [text, setText] = useState('')  
+  const [rating, setRating] = useState(10)  
   const [btnDisabled, setBtnDisabled] = useState(true)  
   const [message, setMessage] = useState('')  
 
@@ -22,11 +24,25 @@ function FeedbackForm() {
     setText(e.target.value)
   }
 
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    if (text.trim().length > 10) {
+        const newFeedBack = {
+            text,
+            rating
+        }
+
+        handleAdd(newFeedBack) 
+
+        setText('')
+    }
+  }
+
   return (
     <Card>
-        <form>
+        <form onSubmit={handleSubmit}>
             <h2>How would you rate your service with us?</h2>
-            { /* @todo - rating selectcom component */ }
+            <RatingSelect select={(rating) => setRating(rating)} />
             <div className="imput-group">
                 <input 
                 onChange={handleTextChange} 
